e58f901af116649da8865dfc9eb80046bdef3b58
K <redknclarke@gmail.com>
2023-11-06 15:09:12 -0500

    added edit functionality

--- sketch.js
+++ sketch.js
@@ -2,25 +2,44 @@ let fieldImage;
 let positions = [];
 const container = document.getElementById("container");
 const outputText = document.getElementById("output-text");
+let circleSize;
+
+const ADD_MODE = 0;
+const EDIT_MODE = 1;
+let state = ADD_MODE;
+
+let selectedPoint = null;
+
 function setup() {
     createCanvas(window.innerHeight/1.5, window.innerHeight/1.5);
     fieldImage = loadImage("vex_field.png");
     container.append(canvas);
     outputText.style.width = `${width*.69}px`
     outputText.style.maxHeight = `${height}px`
+    circleSize = width / 60;
+    ellipseMode(RADIUS);
 }
 
 function draw() {
     image(fieldImage, 0, 0, width, height);
+    drawPoints();
+    getAutonCode();
+}
+
+function drawPoints(){
     if (positions.length <= 0) {
         return;
     }
-
     for (let i = 0; i < positions.length; i++) {
         const pos = positions[i];
-        fill(100, 0, 255);
+        if (pos!=selectedPoint){
+            fill(100, 0, 255);
+        }
+        else{
+            fill(255, 255, 255);
+        }
         noStroke();
-        ellipse(mapToImage(pos.x), mapToImage(pos.y), width/30);
+        ellipse(mapToImage(pos.x), mapToImage(pos.y), circleSize);
         if (i > 0) {
             stroke(255, 0, 100);
             strokeWeight(width/150);
@@ -29,17 +48,71 @@ function draw() {
     }
 }
 
+function addPoint(x, y){
+    positions.push(createVector(mapToField(x), mapToField(y)));
+    selectedPoint = positions[positions.length-1];
+}
+
+function checkPointExists(x, y){
+    for (let i = 0; i < positions.length; i++) {
+        const pos = positions[i];
+        if(pos.x == x && pos.y == y){
+            return i;
+        }
+        if (pos.x + circleSize >= x && pos.x - circleSize <= x && pos.y + circleSize >= y && pos.y - circleSize <= y){
+            return i;
+        }
+    }
+    return false;
+}
+
 function mapToImage(val){
     return map(val, 0, 288, 0, width);
 }
+
 function mapToField(val){
     return map(val, 0, width, 0, 288);
 }
 
-function mousePressed() {
+function mousePressedAddMode(){
     if(mouseX<=width && mouseX >= 0&& mouseY <= height && mouseY >= 0){
-        positions.push(createVector(mapToField(mouseX), mapToField(mouseY)));
-        getAutonCode();
+        addPoint(mouseX,mouseY);
+    }
+}
+
+function mousePressedEditMode(){
+    index = checkPointExists(mapToField(mouseX), mapToField(mouseY));
+    if(index !== false){
+        selectedPoint = positions[index];
+    }
+}
+
+function mousePressed() {
+    if(state==ADD_MODE){
+        mousePressedAddMode();
+    } 
+    else if (state==EDIT_MODE){
+        mousePressedEditMode();
+    } 
+}
+
+function mouseDraggedEditMode(){
+    selectedPoint.x = mapToField(mouseX);
+    selectedPoint.y = mapToField(mouseY);
+}
+
+function mouseDragged(){
+    if(state==EDIT_MODE){
+        mouseDraggedEditMode();
+    }
+}
+
+function keyPressed(){
+    if(key == "e"){
+        state = EDIT_MODE;
+    }
+    if (key=="a"){
+        state = ADD_MODE;
     }
 }
 
@@ -61,6 +134,5 @@ function getAutonCode(){
         // yes this is ugly and no i wont change it :3
         txt += (`chassis.set_turn_pid(${calculateAngleBetweenPoints(pos.x, pos.y, positions[i-1].x, positions[i-1].y)}, TURN_SPEED);\nchassis.wait_drive();\nchassis.set_drive_pid(${floor(p5.Vector.dist(pos, positions[i-1]))}, DRIVE_SPEED);\nchassis.wait_drive();\n\n`);
     }
-    console.log(txt);
     outputText.innerText = txt;
 }
