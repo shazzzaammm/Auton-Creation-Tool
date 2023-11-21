d704077b6d192e0fe158e3e8bf6531db2290e055
Spyder <gaureshgjain@gmail.com>
2023-11-21 11:35:58 -0500

    re implemented straight lines as low res curves

--- sketch.js
+++ sketch.js
@@ -20,9 +20,10 @@ let showRobot = false;
 
 let showHelpBox = true;
 
-let selectedPoint = null;
-
 let spline = new Spline();
+
+let selectedPoint = null;
+let selectedCurve = spline.curves[0];
 
 let robotPosition;
 let robotAngle = 0;
@@ -34,293 +35,290 @@ const ROBOT_TURNING = 1;
 let robotState = ROBOT_DRIVING;
 
 function setup() {
-    createCanvas(
-        min(window.innerHeight, window.innerWidth) / 1.5,
-        min(window.innerHeight, window.innerWidth) / 1.5
-    );
-    fieldImage = loadImage("vex_field.png");
-    chassisImage = loadImage("chassis.png");
-    container.append(canvas);
-    outputText.style.width = `${width * 0.69}px`;
-    outputText.style.maxHeight = `${height}px`;
-    circleSize = width / 60;
-    ellipseMode(RADIUS);
+  createCanvas(
+    min(window.innerHeight, window.innerWidth) / 1.5,
+    min(window.innerHeight, window.innerWidth) / 1.5
+  );
+  fieldImage = loadImage("vex_field.png");
+  chassisImage = loadImage("chassis.png");
+  container.append(canvas);
+  outputText.style.width = `${width * 0.69}px`;
+  outputText.style.maxHeight = `${height}px`;
+  circleSize = width / 60;
+  ellipseMode(RADIUS);
+
+  robotPosition = createVector(
+    spline.curves[0].point1.x,
+    spline.curves[0].point1.y
+  );
 }
 function setLineDash(list) {
-    drawingContext.setLineDash(list);
+  drawingContext.setLineDash(list);
 }
 
 function draw() {
-    imageMode(CORNER);
-    image(fieldImage, 0, 0, width, height);
-    spline.draw();
-    positions = spline.getPoints();
-    if (showLines) {
-        drawLines();
-    }
-    if (showDots) {
-        drawPoints();
-    }
-    if (showRobot || state == ANIMATION_MODE) {
-        drawRobot();
-    }
-    if (state == ANIMATION_MODE) {
-        moveRobot();
-    }
-    getAutonCode();
+  imageMode(CORNER);
+  image(fieldImage, 0, 0, width, height);
+  positions = spline.getPoints();
+  spline.draw();
+  if (showLines) {
+    drawLines();
+  }
+  if (showDots) {
+    drawPoints();
+  }
+  if (showRobot || state == ANIMATION_MODE) {
+    drawRobot();
+  }
+  if (state == ANIMATION_MODE) {
+    moveRobot();
+  }
+  getAutonCode();
 }
 
 function drawPoint(p) {
-    if (p != selectedPoint) {
-        noStroke();
-    } else {
-        stroke(255);
-        strokeWeight(width / 150);
-    }
-    fill(0, 70, 254);
-    ellipse(mapToImage(p.x), mapToImage(p.y), circleSize);
+  if (p != selectedPoint) {
+    noStroke();
+  } else {
+    stroke(255);
+    strokeWeight(width / 150);
+  }
+  fill(0, 70, 254);
+  ellipse(mapToImage(p.x), mapToImage(p.y), circleSize);
 }
 
 function lineBetweenPoints(a, b) {
-    stroke(255, 255, 0);
-    strokeWeight(width / 150);
-    line(mapToImage(a.x), mapToImage(a.y), mapToImage(b.x), mapToImage(b.y));
+  stroke(255, 255, 0);
+  strokeWeight(width / 150);
+  line(mapToImage(a.x), mapToImage(a.y), mapToImage(b.x), mapToImage(b.y));
 }
 
 function drawPoints() {
-    if (positions.length <= 0) {
-        return;
-    }
-    for (let i = 0; i < positions.length; i++) {
-        drawPoint(positions[i]);
-    }
+  if (positions.length <= 0) {
+    return;
+  }
+  for (let i = 0; i < positions.length; i++) {
+    drawPoint(positions[i]);
+  }
 }
 
 function drawLines() {
-    if (positions.length <= 1) {
-        return;
-    }
-    for (let i = 1; i < positions.length; i++) {
-        lineBetweenPoints(positions[i], positions[i - 1]);
-    }
+  if (positions.length <= 1) {
+    return;
+  }
+  for (let i = 1; i < positions.length; i++) {
+    setLineDash([1]);
+    lineBetweenPoints(positions[i], positions[i - 1]);
+  }
 }
 
 function drawRobot() {
-    push();
-    imageMode(CENTER);
-    translate(mapToImage(robotPosition.x), mapToImage(robotPosition.y));
-    if (robotTargetIndex > 0) rotate(radians(robotAngle));
-    image(
-        chassisImage,
-        0,
-        0,
-        mapToImage(18 * robotScale),
-        mapToImage(18 * robotScale)
-    );
-    pop();
+  push();
+  imageMode(CENTER);
+  translate(mapToImage(robotPosition.x), mapToImage(robotPosition.y));
+  if (robotTargetIndex > 0) rotate(radians(robotAngle));
+  image(
+    chassisImage,
+    0,
+    0,
+    mapToImage(18 * robotScale),
+    mapToImage(18 * robotScale)
+  );
+  pop();
 }
 
 function addPoint(x, y) {
-    const point = createVector(mapToField(x), mapToField(y));
-    const index = positions.indexOf(selectedPoint);
+  const point = createVector(mapToField(x), mapToField(y));
+  const index = positions.indexOf(selectedPoint);
 
-    positions.splice(index + 1, 0, point);
+  positions.splice(index + 1, 0, point);
 
-    selectedPoint = point;
-    robotPosition = positions[0].copy();
+  selectedPoint = point;
+  robotPosition = positions[0].copy();
 }
 
-function deletePoint() {
-    positions.splice(positions.indexOf(selectedPoint), 1);
-    selectedPoint = null;
+function deleteCurve() {
+  let index = spline.curves.indexOf(selectedCurve);
+  if (index > 0 && index < spline.curves.length - 1) {
+    spline.curves[index - 1].point2 = spline.curves[index + 1].point1;
+    spline.curves[index - 1].controlPoints[3] = spline.curves[index - 1].point2;
+  }
+  if (index > 0) {
+    selectedCurve = null;
+    spline.curves.splice(index, 1);
+  }
 }
 
 function checkPointExists(x, y) {
-    for (let i = 0; i < positions.length; i++) {
-        const pos = positions[i];
-        if (
-            pos.x + circleSize >= x &&
-            pos.x - circleSize <= x &&
-            pos.y + circleSize >= y &&
-            pos.y - circleSize <= y
-        ) {
-            return i;
-        }
+  for (let i = 0; i < positions.length; i++) {
+    const pos = positions[i];
+    if (
+      pos.x + circleSize >= x &&
+      pos.x - circleSize <= x &&
+      pos.y + circleSize >= y &&
+      pos.y - circleSize <= y
+    ) {
+      return i;
     }
-    return false;
+  }
+  return false;
 }
 
 function mapToImage(val) {
-    return map(val, 0, 288, 0, width);
+  return map(val, 0, 288, 0, width);
 }
 
 function mapToField(val) {
-    return map(val, 0, width, 0, 288);
+  return map(val, 0, width, 0, 288);
 }
 
 function moveRobot() {
-    if (robotState == ROBOT_DRIVING) {
-        // targetPos = positions[robotTargetIndex].copy();
-        targetPos = createVector(
-            positions[robotTargetIndex].x,
-            positions[robotTargetIndex].y
-        );
-        if (
-            p5.Vector.equals(targetPos, robotPosition) &&
-            robotTargetIndex + 1 < positions.length
-        ) {
-            robotTargetIndex++;
-            robotState = ROBOT_TURNING;
-        }
-        robotPosition = robotPosition.add(
-            targetPos.sub(robotPosition).limit(1.25)
-        );
+  if (robotState == ROBOT_DRIVING) {
+    // targetPos = positions[robotTargetIndex].copy();
+    targetPos = createVector(
+      positions[robotTargetIndex].x,
+      positions[robotTargetIndex].y
+    );
+    if (
+      p5.Vector.equals(targetPos, robotPosition) &&
+      robotTargetIndex + 1 < positions.length
+    ) {
+      robotTargetIndex++;
+      robotState = ROBOT_TURNING;
     }
-    if (robotState == ROBOT_TURNING) {
-        pos1 = positions[robotTargetIndex];
-        pos2 = positions[robotTargetIndex - 1];
-        targetAngle = calculateAngleBetweenPoints(
-            pos1.x,
-            pos1.y,
-            pos2.x,
-            pos2.y
-        );
-        robotAngle = lerp(robotAngle, targetAngle, 0.125);
-        if (targetAngle - 1 < robotAngle && robotAngle < targetAngle + 1) {
-            robotState = ROBOT_DRIVING;
-            robotAngle = targetAngle;
-        }
+    robotPosition = robotPosition.add(targetPos.sub(robotPosition).limit(1.25));
+  }
+  if (robotState == ROBOT_TURNING) {
+    pos1 = positions[robotTargetIndex];
+    pos2 = positions[robotTargetIndex - 1];
+    targetAngle = calculateAngleBetweenPoints(pos1.x, pos1.y, pos2.x, pos2.y);
+    robotAngle = lerp(robotAngle, targetAngle, 0.125);
+    if (targetAngle - 1 < robotAngle && robotAngle < targetAngle + 1) {
+      robotState = ROBOT_DRIVING;
+      robotAngle = targetAngle;
     }
+  }
 }
 
 function calculateAngleBetweenPoints(x1, y1, x2, y2) {
-    // Calculate the angle in radians
-    const deltaX = x2 - x1;
-    const deltaY = y2 - y1;
-    const angleRadians = Math.atan2(deltaY, deltaX);
+  // Calculate the angle in radians
+  const deltaX = x2 - x1;
+  const deltaY = y2 - y1;
+  const angleRadians = Math.atan2(deltaY, deltaX);
 
-    // Convert the angle from radians to degrees
-    const angleDegrees = floor((angleRadians * 180) / Math.PI);
-    return floor(angleDegrees - 90);
+  // Convert the angle from radians to degrees
+  const angleDegrees = floor((angleRadians * 180) / Math.PI);
+  return floor(angleDegrees - 90);
 }
 
 function keyPressed() {
-    key = key.toLocaleLowerCase();
-    if (key == "e") {
-        state = EDIT_MODE;
+  key = key.toLocaleLowerCase();
+  if (key == "e") {
+    state = EDIT_MODE;
+  }
+  if (key == "a") {
+    state = ADD_MODE;
+  }
+  if (key == "w") {
+    state = ANIMATION_MODE;
+    robotTargetIndex = 0;
+  }
+  if (state != ANIMATION_MODE) {
+    if (key == "d") {
+      deleteCurve();
     }
-    if (key == "a") {
-        state = ADD_MODE;
+    if (key == "arrowup" && selectedCurve != null) {
+      selectedCurve.dt *= 0.5;
     }
-    if (key == "w") {
-        state = ANIMATION_MODE;
-        robotTargetIndex = 0;
+    if (key == "arrowdown" && selectedCurve != null) {
+      if (selectedCurve.dt * 2 <= 1) selectedCurve.dt *= 2;
     }
-    if (key == "d" && state != ANIMATION_MODE) {
-        deletePoint();
-    }
-    if (key == "x") {
-        robotScale == 1 ? (robotScale = 2.5) : (robotScale = 1);
-    }
-    if (key == "h") {
-        showHelpBox = !showHelpBox;
-        showHelpBox
-            ? (helpBox.style.visibility = "visible")
-            : (helpBox.style.visibility = "hidden");
-    }
+  }
+  if (key == "x") {
+    robotScale == 1 ? (robotScale = 2.5) : (robotScale = 1);
+  }
+  if (key == "h") {
+    showHelpBox = !showHelpBox;
+    showHelpBox
+      ? (helpBox.style.visibility = "visible")
+      : (helpBox.style.visibility = "hidden");
+  }
+  if (key == "1") {
+    showDots = !showDots;
+  }
+  if (key == "2") {
+    showLines = !showLines;
+  }
+  if (key == "3") {
+    showRobot = !showRobot;
+  }
 
-    if (key == "1") {
-        showDots = !showDots;
-    }
-    if (key == "2") {
-        showLines = !showLines;
-    }
-    if (key == "3") {
-        showRobot = !showRobot;
-    }
-
-    if (key == "c") {
-        copyAutonCode();
-    }
+  if (key == "c") {
+    copyAutonCode();
+  }
 }
 
 function mousePressed() {
-    if(mouseX <= width && mouseX >= 0 && mouseY <= height && mouseY >= 0){
+  if (showHelpBox) return;
+  if (state === ADD_MODE) mousePressedAdd();
+  if (state === EDIT_MODE) mousePressedEdit();
+  if (state === ANIMATION_MODE) return;
 
-        selectedPoint = spline.findClickedPoint(mouseX, mouseY);
-        if (selectedPoint == null) {
-        spline.addCurve(mapToField(mouseX), mapToField(mouseY));
-    }
-    robotPosition = createVector(
-        spline.curves[0].point1.x,
-        spline.curves[0].point1.y
-        );
-    }
+  robotPosition = createVector(
+    spline.curves[0].point1.x,
+    spline.curves[0].point1.y
+  );
+}
+
+function mousePressedEdit() {
+  if (mouseX <= width && mouseX >= 0 && mouseY <= height && mouseY >= 0) {
+    selectedPoint = spline.findClickedPoint(mouseX, mouseY);
+    selectedCurve = spline.findClickedCurve(mouseX, mouseY);
+  }
+}
+
+function mousePressedAdd() {
+  if (mouseX <= width && mouseX >= 0 && mouseY <= height && mouseY >= 0) {
+    spline.addCurve(mapToField(mouseX), mapToField(mouseY));
+    selectedCurve = spline.curves[spline.curves.length - 1];
+  }
 }
 
 function mouseDragged() {
-    if (selectedPoint == null) return;
-    selectedPoint.x = mapToField(mouseX);
-    selectedPoint.y = mapToField(mouseY);
+  if (selectedPoint == null) return;
+  if (state === EDIT_MODE) mouseDraggedEdit();
 }
 
-function mouseReleased(){
-    selectedPoint = null;
+function mouseDraggedEdit() {
+  selectedPoint.x = mapToField(mouseX);
+  selectedPoint.y = mapToField(mouseY);
 }
 
-// function mousePressedAddMode() {
-//     if (mouseX <= width && mouseX >= 0 && mouseY <= height && mouseY >= 0) {
-//         addPoint(mouseX, mouseY);
-//     }
-// }
-
-// function mousePressedEditMode() {
-//     index = checkPointExists(mapToField(mouseX), mapToField(mouseY));
-//     if (index !== false) {
-//         selectedPoint = positions[index];
-//     }
-// }
-
-// function mousePressed() {
-//     if (state == ADD_MODE) {
-//         mousePressedAddMode();
-//     } else if (state == EDIT_MODE) {
-//         mousePressedEditMode();
-//     }
-// }
-
-// function mouseDraggedEditMode() {
-//     selectedPoint.x = mapToField(mouseX);
-//     selectedPoint.y = mapToField(mouseY);
-// }
-
-// function mouseDragged() {
-//     if (state == EDIT_MODE) {
-//         mouseDraggedEditMode();
-//     }
-// }
+function mouseReleased() {
+  selectedPoint = null;
+}
 
 function getAutonCode() {
-    txt = "";
-    for (let i = 1; i < positions.length; i++) {
-        const pos = createVector(positions[i].x, positions[i].y);
-        const pos2 = createVector(positions[i - 1].x, positions[i - 1].y);
-        // yes this is ugly and no i wont change it :3
-        txt += `chassis.set_turn_pid(${calculateAngleBetweenPoints(
-            pos.x,
-            pos.y,
-            pos2.x,
-            pos2.y
-        )}, TURN_SPEED);\nchassis.wait_drive();\nchassis.set_drive_pid(${floor(
-            p5.Vector.dist(pos, pos2)
-        )}, DRIVE_SPEED);\nchassis.wait_drive();\n\n`;
-    }
-    if (txt == "") {
-        txt = "Click to add some points :D";
-    }
-    outputText.innerText = txt;
+  txt = "";
+  for (let i = 1; i < positions.length; i++) {
+    const pos = createVector(positions[i].x, positions[i].y);
+    const pos2 = createVector(positions[i - 1].x, positions[i - 1].y);
+    // yes this is ugly and no i wont change it :3
+    txt += `chassis.set_turn_pid(${calculateAngleBetweenPoints(
+      pos.x,
+      pos.y,
+      pos2.x,
+      pos2.y
+    )}, TURN_SPEED);\nchassis.wait_drive();\nchassis.set_drive_pid(${floor(
+      p5.Vector.dist(pos, pos2)
+    )}, DRIVE_SPEED);\nchassis.wait_drive();\n\n`;
+  }
+  if (txt == "") {
+    txt = "Click to add some points :D";
+  }
+  outputText.innerText = txt;
 }
 
 function copyAutonCode() {
-    navigator.clipboard.writeText(outputText.innerText);
+  navigator.clipboard.writeText(outputText.innerText);
 }
--- spline.js
+++ spline.js
@@ -1,170 +1,205 @@
-const deltaT = 0.1;
-
 class Spline {
-    constructor() {
-        this.curves = [
-            new BezierCurve(
-                new Point(208, 256),
-                new Point(230, 255),
-                new Point(211, 134),
-                new Point(184, 134)
-            ),
-        ];
+  constructor() {
+    this.curves = [
+      new BezierCurve(
+        new Point(208, 256),
+        new Point(230, 255),
+        new Point(211, 134),
+        new Point(184, 134),
+        1
+      ),
+    ];
+  }
+  draw() {
+    for (const curve of this.curves) {
+      curve.draw();
     }
-    draw() {
-        for (const curve of this.curves) {
-            curve.draw();
-        }
+  }
+
+  addCurve(x, y) {
+    let startPoint = this.curves[this.curves.length - 1].point2;
+    let endPoint = new Point(x, y);
+    let controlPoint1 = new Point(
+      (startPoint.x + endPoint.x) / 2,
+      (startPoint.y + endPoint.y) / 2
+    );
+    let controlPoint2 = new Point(
+      (startPoint.x + endPoint.x) / 2,
+      (startPoint.y + endPoint.y) / 2
+    );
+    this.curves.push(
+      new BezierCurve(
+        startPoint,
+        controlPoint1,
+        endPoint,
+        controlPoint2,
+        this.curves[this.curves.length - 1].dt
+      )
+    );
+  }
+
+  findClickedPoint(x, y) {
+    for (const curve of this.curves) {
+      let foundPoint = curve.findClickedPoint(x, y);
+      if (foundPoint !== null) {
+        return foundPoint;
+      }
+    }
+    return null;
+  }
+
+  findClickedCurve(x, y) {
+    for (const curve of this.curves) {
+      let foundPoint = curve.findClickedPoint(x, y);
+      if (foundPoint !== null) {
+        return curve;
+      }
+    }
+    return null;
+  }
+
+  getPoints() {
+    let points = [];
+    for (const curve of this.curves) {
+      points = points.concat(curve.getPoints());
     }
 
-    addCurve(x, y) {
-        let startPoint = this.curves[this.curves.length - 1].point2;
-        let endPoint = new Point(x, y);
-        let controlPoint1 = new Point(
-            (startPoint.x + endPoint.x) / 2,
-            (startPoint.y + endPoint.y) / 2
-        );
-        let controlPoint2 = new Point(
-            (startPoint.x + endPoint.x) / 2,
-            (startPoint.y + endPoint.y) / 2
-        );
-        this.curves.push(
-            new BezierCurve(startPoint, controlPoint1, endPoint, controlPoint2)
-        );
-    }
-
-    findClickedPoint(x, y) {
-        for (const curve of this.curves) {
-            let foundPoint = curve.findClickedPoint(x, y);
-            if (foundPoint !== null) {
-                return foundPoint;
-            }
-        }
-        return null;
-    }
-
-    getPoints() {
-        let points = [];
-        for (const curve of this.curves) {
-            points = points.concat(curve.getPoints());
-        }
-        // console.log(points);
-        return points;
-    }
+    return points.filter((item, index) => points.indexOf(item) == index);
+  }
 }
 
 class Point {
-    constructor(x, y) {
-        this.w = 30;
-        this.x = x;
-        this.y = y;
-    }
-    draw() {
-        textSize(this.w);
-        textStyle(BOLD);
-        textAlign(CENTER, CENTER);
-        text("X", mapToImage(this.x), mapToImage(this.y));
-    }
-    isClicked(x, y) {
-        
-        return (
-            x <= mapToImage(this.x) + this.w &&
-            x >= mapToImage(this.x) - this.w &&
-            y <= mapToImage(this.y) + this.w &&
-            y >= mapToImage(this.y) - this.w
-        );
-    }
+  constructor(x, y) {
+    this.w = 30;
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    textSize(this.w);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    text("X", mapToImage(this.x), mapToImage(this.y));
+  }
+  isClicked(x, y) {
+    return (
+      x <= mapToImage(this.x) + this.w / 2 &&
+      x >= mapToImage(this.x) - this.w / 2 &&
+      y <= mapToImage(this.y) + this.w / 2 &&
+      y >= mapToImage(this.y) - this.w / 2
+    );
+  }
+
+  copy() {
+    return new Point(this.x, this.y);
+  }
 }
 
 class BezierCurve {
-    constructor(point1, control1, point2, control2) {
-        this.point1 = point1;
-        this.point2 = point2;
-        this.control1 = control1;
-        this.control2 = control2;
-        this.controlPoints = [point1, control1, control2, point2];
-        this.curvePoints = [];
-    }
+  constructor(point1, control1, point2, control2, deltaT) {
+    this.dt = deltaT;
+    this.point1 = point1;
+    this.point2 = point2;
+    this.control1 = control1;
+    this.control2 = control2;
+    this.controlPoints = [point1, control1, control2, point2];
+  }
 
-    draw() {
-        this.drawPoints();
-        // this.drawCurve();
-        this.drawHandles();
+  draw() {
+    this.drawPoints();
+    if (this == selectedCurve && showLines) {
+      this.drawCurve();
     }
+    if (this.dt != 1) {
+      this.drawHandles();
+    }
+  }
 
-    binomialCoefficient(n, k) {
-        if (k === 0 || k === n) {
-            return 1;
-        } else {
-            return (
-                this.binomialCoefficient(n - 1, k - 1) +
-                this.binomialCoefficient(n - 1, k)
-            );
-        }
+  binomialCoefficient(n, k) {
+    if (k === 0 || k === n) {
+      return 1;
+    } else {
+      return (
+        this.binomialCoefficient(n - 1, k - 1) +
+        this.binomialCoefficient(n - 1, k)
+      );
     }
+  }
 
-    bezierCurve(t, controlPoints) {
-        const n = controlPoints.length - 1;
-        let result = new Point(0, 0);
+  bezierCurve(t, controlPoints) {
+    const n = controlPoints.length - 1;
+    let result = new Point(0, 0);
 
-        for (let i = 0; i <= n; i++) {
-            const coefficient =
-                this.binomialCoefficient(n, i) *
-                Math.pow(1 - t, n - i) *
-                Math.pow(t, i);
-            result.x += coefficient * controlPoints[i].x;
-            result.y += coefficient * controlPoints[i].y;
-        }
-        return result;
+    for (let i = 0; i <= n; i++) {
+      const coefficient =
+        this.binomialCoefficient(n, i) *
+        Math.pow(1 - t, n - i) *
+        Math.pow(t, i);
+      result.x += coefficient * controlPoints[i].x;
+      result.y += coefficient * controlPoints[i].y;
     }
+    return result;
+  }
 
-    drawPoints() {
-        noStroke();
-        for (let i = 0; i < this.controlPoints.length; i++) {
-            const pt = this.controlPoints[i];
-            if (pt == this.control1 || pt == this.control2) {
-                fill(255);
-            } else {
-                fill(255, 105, 180);
-            }
-            pt.draw();
-        }
+  drawPoints() {
+    if (this.dt == 1) {
+      return;
     }
+    noStroke();
+    for (let i = 0; i < this.controlPoints.length; i++) {
+      const pt = this.controlPoints[i];
+      if (pt == this.control1 || pt == this.control2) {
+        fill(255);
+      } else {
+        fill(128, 53, 90);
+      }
+      pt.draw();
+    }
+  }
 
-    drawCurve() {
-        noFill();
-        strokeWeight(5);
-        setLineDash([1]);
-        beginShape();
-        for (let t = 0; t <= 1; t += deltaT) {
-            const point = this.bezierCurve(t, this.controlPoints);
-            stroke(100, 0, 255);
-            vertex(mapToImage(point.x), mapToImage(point.y));
-        }
-        endShape();
+  drawCurve() {
+    noFill();
+    strokeWeight(6);
+    setLineDash([1]);
+    beginShape();
+    stroke(0);
+    for (let t = 0; t <= 1; t += this.dt) {
+      const point = this.bezierCurve(t, this.controlPoints);
+      vertex(mapToImage(point.x), mapToImage(point.y));
     }
+    endShape();
+  }
 
-    drawHandles() {
-        setLineDash([10, 10]);
-        stroke(255, 0, 100);
-        strokeWeight(2);
-        line(mapToImage(this.control1.x), mapToImage(this.control1.y), mapToImage(this.point1.x), mapToImage(this.point1.y));
-        line(mapToImage(this.control2.x), mapToImage(this.control2.y), mapToImage(this.point2.x), mapToImage(this.point2.y));
-    }
+  drawHandles() {
+    setLineDash([10, 10]);
+    stroke(255, 0, 100);
+    strokeWeight(2);
+    line(
+      mapToImage(this.control1.x),
+      mapToImage(this.control1.y),
+      mapToImage(this.point1.x),
+      mapToImage(this.point1.y)
+    );
+    line(
+      mapToImage(this.control2.x),
+      mapToImage(this.control2.y),
+      mapToImage(this.point2.x),
+      mapToImage(this.point2.y)
+    );
+  }
 
-    findClickedPoint(x, y) {
-        for (const pt of this.controlPoints) {
-            if (pt.isClicked(x, y) === true) {
-                return pt;
-            }
-        }
-        return null;
+  findClickedPoint(x, y) {
+    for (const pt of this.controlPoints) {
+      if (pt.isClicked(x, y) === true) {
+        return pt;
+      }
     }
-    getPoints(){
-        const points = [];
-        for (let t = 0; t <= 1; t += deltaT) {
-            points.push(this.bezierCurve(t, this.controlPoints));
-        }
-        return points;
+    return null;
+  }
+  getPoints() {
+    const points = [];
+    for (let t = 0; t <= 1; t += this.dt) {
+      points.push(this.bezierCurve(t, this.controlPoints));
     }
+    return points;
+  }
 }
